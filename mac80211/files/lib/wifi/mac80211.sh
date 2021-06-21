182981b18f18d6eb51c0b8970daab67b5d0c5f4e
Chad Monroe <chad.monroe@smartrg.com>
2021-06-21 15:13:54 -0700

    OWRT-5641 mac80211: if radio supports dual band mode, default to first mode (2.4G).

--- mac80211/files/lib/wifi/mac80211.sh
+++ mac80211/files/lib/wifi/mac80211.sh
@@ -132,6 +132,10 @@ get_band_defaults() {
 		mode_band="$band"
 		channel="$chan"
 		htmode="$mode"
+
+		# use only the first returned mode to we handle boards that
+		# say they support concurrent dual band by defaulting to 2.4G
+		break
 	done
 }
 
